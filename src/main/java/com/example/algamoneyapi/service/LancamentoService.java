e2d6eb1a6408b0c85f3f31e22369c5f6954e6304
Wisley <wisleysiqueira@hotmail.com>
2021-01-06 22:04:21 -0300

    Primeiro commit. Recursos e segurança.

---
+++ src/main/java/com/example/algamoneyapi/service/LancamentoService.java
@@ -0,0 +1,31 @@
+package com.example.algamoneyapi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoneyapi.model.Lancamento;
+import com.example.algamoneyapi.model.Pessoa;
+import com.example.algamoneyapi.repository.LancamentoRepository;
+import com.example.algamoneyapi.repository.PessoaRepository;
+import com.example.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired 
+	private LancamentoRepository lancamentoRepository;
+	
+	public Lancamento salvar(Lancamento lancamento) {
+		Optional<Pessoa> pessoaOpt = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+
+	    if (!pessoaOpt.isPresent() || pessoaOpt.get().isInativo()) {
+	        throw new PessoaInexistenteOuInativaException();
+		}
+		return lancamentoRepository.save(lancamento);
+	}
+}
